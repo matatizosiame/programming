adb1a498401808721dae793827a471cfb332aa5f
matatizosiame <matatizosiame@gmail.com>
2022-09-15 12:27:42 -0700

    C Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints positive or negative integer
+ * Retuen: always 0
+ */
+
+void positive_or_negative(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
+
+
