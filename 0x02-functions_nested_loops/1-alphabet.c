a84d74a1ee83f64d2e234979c3294436df87dbb9
matatizosiame <matatizosiame@gmail.com>
2022-09-15 04:37:25 -0700

    C functions nested ioops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - use putchar twice
+ *
+ *Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int fa;
+
+	for (fa = 'a'; fa <= 'z'; fa++)
+	{
+		_putchar(fa);
+		_putchar('\n');
+	}
+
+	return (0);
+}
+
